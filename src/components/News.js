0133b83c8a40411a1a0db334971364ed21ab1a68
Yogesh Summan <yogeshsumman2001@gmail.com>
2023-06-28 23:14:37 +0530

    new changes

--- src/components/News.js
+++ src/components/News.js
@@ -5,17 +5,17 @@ export class News extends Component {
     articles = [
         {
             "source": {
-                "id": "bbc-sport",
-                "name": "BBC Sport"
+              "id": null,
+              "name": "Biztoc.com"
             },
-            "author": "BBC Sport",
-            "title": "Shane Warne memorial - watch & follow updates",
-            "description": "Watch live coverage and follow text updates and tributes from the state memorial for Australian cricket legend Shane Warne at the Melbourne Cricket Ground.",
-            "url": "http://www.bbc.co.uk/sport/live/cricket/60916236",
-            "urlToImage": "https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.22.0/images/bbc-sport-logo.png",
-            "publishedAt": "2022-03-30T08:22:26.498888Z",
-            "content": "Former England bowler and BBC cricket presenter Isa Guha, who became a colleague of Warne's in the commentary box: \"It has been a strange few weeks - a lot of shock and then we did our own tribute at… [+396 chars]"
-        },
+            "author": "barrons.com",
+            "title": "Tesla, Ford News Makes a Point: Don't Overreact to Recalls",
+            "description": "The latest news about auto recalls underscores a point Barron’s has been making for years: The vast majority of instances don’t affect car makers’ stocks, or their earnings.#barrons",
+            "url": "https://biztoc.com/x/3260fa214fc2a685",
+            "urlToImage": "https://c.biztoc.com/p/3260fa214fc2a685/og.webp",
+            "publishedAt": "2023-06-26T16:52:07Z",
+            "content": "The latest news about auto recalls underscores a point Barrons has been making for years: The vast majority of instances dont affect car makers stocks, or their earnings.\r\n#barrons\r\nThis story appear… [+19 chars]"
+          },
         {
             "source": {
                 "id": "espn-cric-info",
@@ -41,7 +41,46 @@ export class News extends Component {
             "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
             "publishedAt": "2020-03-30T15:26:05Z",
             "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
+        },
+        {
+            "source": {
+              "id": null,
+              "name": "Biztoc.com"
+            },
+            "author": "finance.yahoo.com",
+            "title": "The stock market's biggest winners are getting downgraded by Wall Street",
+            "description": "Tech stocks have rebounded so well from their down year in 2022 that there might not a buying opportunity there anymore. At least that's what some Wall Street analysts are saying as they slap downgrades on some of 2023's best performers like Tesla (TSLA), App…",
+            "url": "https://biztoc.com/x/80d0ed065588693e",
+            "urlToImage": "https://c.biztoc.com/p/80d0ed065588693e/og.webp",
+            "publishedAt": "2023-06-26T16:38:09Z",
+            "content": "Tech stocks have rebounded so well from their down year in 2022 that there might not a buying opportunity there anymore.At least that's what some Wall Street analysts are saying as they slap downgrad… [+286 chars]"
+          },
+        {
+            "source": {
+              "id": null,
+              "name": "Snopes.com"
+            },
+            "author": "Aleksandra Wrona",
+            "title": "Is This an Authentic Video of a Tesla Catching on Fire?",
+            "description": "Videos of Teslas catching on fire are often shared on social media, though some of them might be misleading.",
+            "url": "https://www.snopes.com/fact-check/tesla-on-fire/",
+            "urlToImage": "https://mediaproxy.snopes.com/width/1200/https://media.snopes.com/2023/06/tesla_on_fire_miscaptioned.png",
+            "publishedAt": "2023-06-26T17:31:00Z",
+            "content": "Claim:\r\nA video tweeted on June 26, 2023, authentically showed a Tesla catching on fire. \r\nOn June 26, 2023, Twitter user @Alphafox78 posted a one-minute long video, claiming that it showed a Tesla c… [+2684 chars]"
+          },
+          {
+            "source": {
+              "id": null,
+              "name": "Yahoo Entertainment"
+            },
+            "author": "Nate Doughty, Pittsburgh Business Times",
+            "title": "Pittsburgh Regional Transit approves $535 million budget, not expecting ‘major service reductions’",
+            "description": "The board of the region’s largest transportation authority approved an operating budget of $535.4 million on Friday for its upcoming fiscal year, which is a ...",
+            "url": "https://news.yahoo.com/pittsburgh-regional-transit-approves-535-173549516.html",
+            "urlToImage": "https://media.zenfs.com/en/wpxi_cox_articles_540/8fad34e021b3caf7429ee2cb758dcd0b",
+            "publishedAt": "2023-06-26T17:35:49Z",
+            "content": "The board of the regions largest transportation authority approved an operating budget of $535.4 million on Friday for its upcoming fiscal year, which is a 1.8% increase from the prior years budget.\r… [+946 chars]"
+          }
     ]
     constructor() {
         super();
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -10,7 +10,7 @@ export class Newsitem extends Component {
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
-                        <a href={newsUrl} target="_blank" className="btn btn-primary">Go somewhere</a>
+                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Go somewhere</a>
                     </div>
                 </div>
             </div>
